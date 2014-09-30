33c31efd501ffa3ec869fac75ce78b2fb597c596
czengg <czengg@Cindys-MacBook-Pro-2.local>
2014-09-30 16:20:44 -0400

    not sure

--- public/js/controllers/dishesController.js
+++ public/js/controllers/dishesController.js
@@ -1,20 +1,9 @@
-var DishesController = function () {
+var DishesController = function ($http) {
     this.here = "hi";
     this.restaurant = {};
-    
-    var url = 'http://localhost:8080/getrestaurant/lulus';
-    var that = this;
-    
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4) {
-            that.restaurant = xmlhttp.responseText;
-        }
-    }
-    
-    xmlhttp.open('GET', url, true);
-    xmlhttp.send();
-    console.log('here');
+    var self = this;
+    var url = 'http://localhost:8080/getrestaurant/lulus';        
+    $http.get(url).success(function(data) {self.restaurant = data});
 };    
 
 var controllerCall = angular.module('top5', ['ngAnimate'])
