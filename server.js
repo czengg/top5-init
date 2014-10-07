01e361f4da7dcabd801e55a3b43f8f86c55ec96c
Jay Chopra <jchopra@andrew.cmu.edu>
2014-10-07 02:38:56 -0400

    fdjskhf

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ router.get('/', function (req, res) {
 });
 
 
-router.get('/userlist', function(req, res) {
+router.get('/', function(req, res) {
     var db = req.db;
     var collection = db.get('usercollection');
     collection.find({},{},function(e,docs){
@@ -38,64 +38,71 @@ router.get('/userlist', function(req, res) {
 });
 
 
-// router.get('/getrestaurant/:long/:lat', function(req, res) {
-//     var restaurant = {};
-//     restaurant.dishes = [
-//         {
-//             id          : 0,
-//             type        : "food",
-//             name        : "Pad Thai",
-//             price       : 7.25,
-//             description : "Thai rice noodles stir fried in a special thai sauce with egg, tofu, bean sprouts, green onions, and chopped peanuts, then garnished with bean sprouts and red cabbage.",
-//             favorited   : true,
-//             likeRank    : 2
-//         },
-//         {
-//             id          : 1,
-//             type        : "food",
-//             name        : "Singapore Rice Noodle",
-//             price       : 7.25,
-//             description : "Vermicelli rice noodles stir fried in light curry with shrimp, chicken, bean sprouts, onion and eggs.",
-//             favorited   : true,
-//             likeRank    : 1
-//         },
-//         {
-//             id          : 2,
-//             type        : "food",
-//             name        : "Beef Chow Fun",
-//             price       : 7.25,
-//             description : "Fresh wide rice noodles, bean sprouts, green onions, stir fried in specia sauce handed down from ma ma's recipes.",
-//             favorited   : false,
-//             likeRank    : 3
-//         },
-//         {
-//             id          : 3,
-//             type        : "food",
-//             name        : "Traditional Fried Rice",
-//             price       : 6.50,
-//             description : "Seasoned rice, green peas, carrot, onion, egg, your choice of meats.",
-//             favorited   : false,
-//             likeRank    : 4
-//         }, 
-//         {
-//             id          : 4,
-//             type        : "drink",
-//             name        : "Fresh Mango Bubble Tea",
-//             price       : 7.25,
-//             description : "Milk Tea with Bubbles made with Fresh Mango",
-//             favorited   : true,
-//             likeRank    : 0
-//         }
-//     ];
-//     restaurant.id = 0;
-//     restaurant.selected = restaurant.dishes[0].name;
-//     restaurant.name = "Lulu's Noodle House";
-//     res.send(JSON.stringify(restaurant));
-// });
+router.get('/getrestaurant/:long/:lat', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+    // var restaurant = {};
+    // restaurant.dishes = [
+    //     {
+    //         id          : 0,
+    //         type        : "food",
+    //         name        : "Pad Thai",
+    //         price       : 7.25,
+    //         description : "Thai rice noodles stir fried in a special thai sauce with egg, tofu, bean sprouts, green onions, and chopped peanuts, then garnished with bean sprouts and red cabbage.",
+    //         favorited   : true,
+    //         likeRank    : 2
+    //     },
+    //     {
+    //         id          : 1,
+    //         type        : "food",
+    //         name        : "Singapore Rice Noodle",
+    //         price       : 7.25,
+    //         description : "Vermicelli rice noodles stir fried in light curry with shrimp, chicken, bean sprouts, onion and eggs.",
+    //         favorited   : true,
+    //         likeRank    : 1
+    //     },
+    //     {
+    //         id          : 2,
+    //         type        : "food",
+    //         name        : "Beef Chow Fun",
+    //         price       : 7.25,
+    //         description : "Fresh wide rice noodles, bean sprouts, green onions, stir fried in specia sauce handed down from ma ma's recipes.",
+    //         favorited   : false,
+    //         likeRank    : 3
+    //     },
+    //     {
+    //         id          : 3,
+    //         type        : "food",
+    //         name        : "Traditional Fried Rice",
+    //         price       : 6.50,
+    //         description : "Seasoned rice, green peas, carrot, onion, egg, your choice of meats.",
+    //         favorited   : false,
+    //         likeRank    : 4
+    //     }, 
+    //     {
+    //         id          : 4,
+    //         type        : "drink",
+    //         name        : "Fresh Mango Bubble Tea",
+    //         price       : 7.25,
+    //         description : "Milk Tea with Bubbles made with Fresh Mango",
+    //         favorited   : true,
+    //         likeRank    : 0
+    //     }
+    // ];
+    // restaurant.id = 0;
+    // restaurant.selected = restaurant.dishes[0].name;
+    // restaurant.name = "Lulu's Noodle House";
+    // res.send(JSON.stringify(restaurant));
+});
 
-// router.get('/updatefavorite/:restaurant/:dish', function(req, res) {
-//     res.send(req.params.dish);
-// });
+router.get('/updatefavorite/:restaurant/:dish', function(req, res) {
+    res.send(req.params.dish);
+});
 
 app.use('/', router);
 
